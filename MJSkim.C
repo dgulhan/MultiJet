c54bc28ebf9db472b8227953795e1eae51b6a9fd
Edna Ruiz <eruiz@ednasmac.dyndns.cern.ch>
2016-06-23 10:02:55 +0200

    MJSKim for n jets first try

--- MJSkim.C
+++ MJSkim.C
@@ -121,7 +121,7 @@ void MJSkim(TString dataset = "/mnt/hadoop/cms/store/user/abaty/transferTargetDi
     for (Long64_t jentry=0; jentry<nentries;jentry++) {
 
         if(nentries%1000==jentry)cout << jentry << endl;
-            // cout << jentry << endl;
+        cout << jentry << endl;
   
         // t->GetEntry(jentry);
         fhi->GetEntry(jentry);
