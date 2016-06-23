693899285864e2ec71ed0a240824a592bde6c5bf
Edna Ruiz <eruiz@ednasmac.dyndns.cern.ch>
2016-06-23 16:55:10 +0200

    run.sh for pp

--- run.sh
+++ run.sh
@@ -4,10 +4,9 @@ g++ MJSkim.C `fastjet-install/bin/fastjet-config --cxxflags --libs --plugins` $(
 #dir=root://eoscms//eos/cms/store/group/cmst3/group/hintt/mverweij/CS/MC/PbPb/PYTHIA_QCD_TuneCUETP8M1_cfi_GEN_SIM_5020GeV/crab_HiForestDijet80CSV19/160329_171047/0000
 #dir=root://eoscms//eos/cms/store/group/cmst3/group/hintt/mverweij/PP5TeV/data/HighPtJet80/crab_HighPtJet80_v2/160525_095945/mergePartialV2/
 #dir=root://eoscms//eos/cms/store/group/cmst3/group/hintt/mverweij/jetsPbPb/Run2/MC/PP/Pythia6_Dijet100_pp502/local/zcutcombi/v1/
-dir=root://eoscms//eos/cms/store/group/cmst3/group/hintt/mverweij/PP5TeV/data/H\
-ighPtJet80/crab_HighPtJet80_v2/160525_095945/mergePartialV2/
+dir=root://eoscms//eos/cms/store/group/cmst3/group/hintt/mverweij/PP5TeV/data/HighPtJet80/crab_HighPtJet80_v2/160525_095945/mergePartialV2/
 
-infile=HiForestAOD_8.root
+infile=HiForest_0.root
 out=MJSkim_pp_Jet80.root
 
 echo | awk -v dataset=$dir/$infile -v  outfile=$out '{print "./dijetSkim \""dataset"\" \""infile"\" \""outfile"\""}' | bash
