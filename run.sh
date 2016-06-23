4670f8402c00ca4d2bf1bf890b6ced6c0701e5cc
Edna Ruiz <eruiz@ednasmac.dyndns.cern.ch>
2016-06-23 18:23:20 +0200

    run.sh input

--- run.sh
+++ run.sh
@@ -1,3 +1,16 @@
+
+
+inFile=
+
+until [ -z "$1" ]; do
+    case "$1" in
+        -i|--input) shift; inFile="$1"; shift ;;
+        -h|--help) shift; echo "sh run.sh -i(--input) HiForest_0.root "; shift;;
+        -*) shift ;;
+        *) break ;;
+    esac
+done
+
 g++ MJSkim_no_N.C `fastjet-install/bin/fastjet-config --cxxflags --libs --plugins` $(root-config --cflags --libs) -lNsubjettiness -Wall -O2 -o dijetSkim
 
 #dir=root://eoscms//eos/cms/store/cmst3/group/hintt/mverweij/jetsPbPb/Run2/MC/PP/Pythia8_Dijet100_pp502_TuneCUETP8M1/crab_HiForestDijet100PP_v4/160510_111948/0000/
@@ -6,7 +19,6 @@ g++ MJSkim_no_N.C `fastjet-install/bin/fastjet-config --cxxflags --libs --plugin
 #dir=root://eoscms//eos/cms/store/group/cmst3/group/hintt/mverweij/jetsPbPb/Run2/MC/PP/Pythia6_Dijet100_pp502/local/zcutcombi/v1/
 dir=root://eoscms//eos/cms/store/group/cmst3/group/hintt/mverweij/PP5TeV/data/HighPtJet80/crab_HighPtJet80_v2/160525_095945/mergePartialV2/
 
-infile=HiForest_0.root
 out=MJSkim_pp_Jet80.root
 
 echo | awk -v dataset=$dir/$infile -v  outfile=$out '{print "./dijetSkim \""dataset"\" \""infile"\" \""outfile"\""}' | bash
