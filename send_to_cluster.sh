dac8d1154ec785a287fd2f936d080a263d067986
Edna Ruiz <eruiz@ednasmac.dyndns.cern.ch>
2016-06-24 10:48:34 +0200

    script for sending jobs to the cluster

--- send_to_cluster.sh
+++ send_to_cluster.sh
@@ -6,8 +6,9 @@ WD=/afs/cern.ch/work/e/eruizvel/private/MuliJetEdna/MultiJet
 indir=/store/group/cmst3/group/hintt/mverweij/PP5TeV/data/HighPtJet80/crab_HighPtJet80_v2/160525_095945/mergePartialV2
 
 echo $indir
+echo "1"
 FILES=$(eos ls $indir)
-
+echo "2"
 for f in ${FILES[@]}
 do
     echo $f
