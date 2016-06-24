776c1f614eea0a52607c5b381f02052b33542e37
Edna Ruiz <eruiz@ednasmac.dyndns.cern.ch>
2016-06-24 09:27:35 +0200

    run for skim

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ until [ -z "$1" ]; do
     esac
 done
 
-g++ MJSkim_no_N.C `fastjet-install/bin/fastjet-config --cxxflags --libs --plugins` $(root-config --cflags --libs) -lNsubjettiness -Wall -O2 -o dijetSkim
+g++ MJSkim.C `fastjet-install/bin/fastjet-config --cxxflags --libs --plugins` $(root-config --cflags --libs) -lNsubjettiness -Wall -O2 -o dijetSkim
 
 #dir=root://eoscms//eos/cms/store/cmst3/group/hintt/mverweij/jetsPbPb/Run2/MC/PP/Pythia8_Dijet100_pp502_TuneCUETP8M1/crab_HiForestDijet100PP_v4/160510_111948/0000/
 #dir=root://eoscms//eos/cms/store/group/cmst3/group/hintt/mverweij/CS/MC/PbPb/PYTHIA_QCD_TuneCUETP8M1_cfi_GEN_SIM_5020GeV/crab_HiForestDijet80CSV19/160329_171047/0000
