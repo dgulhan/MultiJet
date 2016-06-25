2a33a0fa49a9a80e02aa5ef0e038c506449a4d59
Edna Ruiz <eruiz@Ernestos-Air-2.fritz.box>
2016-06-25 10:04:14 +0200

    script for sending jobs to the cluster

--- send_to_cluster.sh
+++ send_to_cluster.sh
@@ -1,21 +1,34 @@
 
+## mounting eos necessary. see https://twiki.cern.ch/twiki/bin/view/TOTEM/CompEOS
 
+HOME=/afs/cern.ch/user/e/eruizvel/private
 WD=/afs/cern.ch/work/e/eruizvel/private/MuliJetEdna/MultiJet
 
+echo "Mounting eos at $HOME"
+eosmount $HOME/eos
 
 
-indir=/store/group/cmst3/group/hintt/mverweij/PP5TeV/data/HighPtJet80/crab_HighPtJet80_v2/160525_095945/mergePartialV2
+indir=
 
-echo $indir
-echo "1"
-FILES=$(eos ls $indir)
-echo "2"
-for f in ${FILES[@]}
-do
-    echo $f
+until [ -z "$1" ]; do
+case "$1" in
+-i|--input) shift; indir="$1"; shift ;;
+-h|--help) shift; echo "sh send_to_cluster.sh -i(--input) /your/dir/wo/eos "; shift;;
+-*) shift ;;
+*) break ;;
+esac
 done
 
-echo "FIN"
+FILES=$(eos ls $indir/*.root)
+
+for f in ${FILES[@]}
+do
+    echo "f"
+done
+
+
+eosforceumount $HOME/eos
+echo "eos unmounted and jobs submited..."
 
 
 
