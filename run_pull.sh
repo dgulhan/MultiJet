19d20e938f6f5b3deb75b7eed8e7d800c895643c
Edna Ruiz <eruiz@ednasmac.dyndns.cern.ch>
2016-07-12 14:35:14 +0200

    all for pull

---
+++ run_pull.sh
@@ -0,0 +1,30 @@
+
+
+indir=
+infile=
+inlabel=
+inmode=
+
+until [ -z "$1" ]; do
+case "$1" in
+-i|--input) shift; infile="$1"; shift ;;
+-d|--dir) shift; indir="$1"; shift;;
+-l|--label) shift; inlabel="$1"; shift;;
+-m|--mode) shift; inmode="$1"; shift;;
+-h|--help) shift; echo "sh run_pull.sh -d(--dir) your/directory -i(--input) HiForest_0.root -l(--label) label for output name -m ppMC or PbPbData f.e "; shift;;
+-*) shift ;;
+*) break ;;
+esac
+done
+
+
+
+WD=/afs/cern.ch/work/e/eruizvel/private/MultiJetEdna/MultiJet
+cd $WD
+
+
+dir=root://eoscms//eos/cms$indir
+
+out=$WD/$inlabel$infile
+
+./dijetPull $dir/$infile $out $inmode
