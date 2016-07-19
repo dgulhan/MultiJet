22c4cfb29c9280bcf121af130d69757c4b079d2c
Edna Ruiz <eruiz@ednasmac.dyndns.cern.ch>
2016-07-19 14:38:39 +0200

    pull pi

--- PullAngle.C
+++ PullAngle.C
@@ -37,7 +37,7 @@ void PullAngle(){
     TFile *file[nFiles];
     TTree *xcR4N3PF[nFiles];
     
-    TCut cut1 = "pt1>120 && pt3>30 && acos(cos(phi1-phi2))>2*TMath::Pi()/3 && sqrt(pow(deltaPhi(phi3,phi2),2.)+pow(eta2-eta3,2.))<0.5" ;
+    TCut cut1 = "pt1>120 && pt3>30 && acos(cos(phi1-phi2))>2*TMath::Pi()/3 && sqrt(pow(deltaPhi(phi3,phi2),2.)+pow(eta2-eta3,2.))<0.7" ;
     
     
     TH1D *h[nFiles];
@@ -47,8 +47,8 @@ void PullAngle(){
     for ( int iFile = 0 ; iFile < nFiles; iFile++ ) {
         
         file[iFile] = TFile::Open(Files[iFile].Data());
-        xcR4N3PF[iFile] = (TTree*)file[iFile]->Get("ak4PF");
-        h[iFile] = new TH1D(Form("h%i",iFile),";#theta_{Pull 2,3};Event fraction",50,0,3.5);
+        xcR4N3PF[iFile] = (TTree*)file[iFile]->Get("xc_R5_N3_PF");
+        h[iFile] = new TH1D(Form("h%i",iFile),"XConeR5N3delta230.7;#theta_{Pull 2,3};Event fraction",50,0,3.5);
         xcR4N3PF[iFile]->Draw(Form("acos((pullEta2*(eta3-eta2)+pullPhi2*deltaPhi(phi3,phi2))/( mag(pullEta2,pullPhi2)*mag(eta3-eta2,deltaPhi(phi3,phi2)) ))>>h%i",iFile),cut1);
         h[iFile]->Scale(1./h[iFile]->Integral());
         h[iFile]->SetLineColor(Colors[iFile]);
