d7f1ad6c0847de7b6ff2b0da49d09c3c209d5130
Edna Ruiz <eruiz@ednasmac.dyndns.cern.ch>
2016-07-21 16:43:35 +0200

    mean pull

--- mean_pull.C
+++ mean_pull.C
@@ -30,6 +30,12 @@ float mag(float x, float y){
 
 
 void mean_pull(){
+    
+    Int_t R[] = {4};
+    Int_t N[] = {3};
+    int nR=1;
+    int nN=1; //XCone
+    
     int nPoints = 6;
     
     TCut CutsR[] = {"sqrt(pow(deltaPhi(phi3,phi2),2.)+pow(eta2-eta3,2.))<0.2",
@@ -55,133 +61,89 @@ void mean_pull(){
     TString Files[] = {"root://eoscms//eos/cms/store/group/cmst3/user/dgulhan/MultiJetSkims/20160720/PbPbMCpthat80+pullHiForestAOD_ALL.root",
                        "/afs/cern.ch/work/e/eruizvel/public/PbPbDataHIHardProbes+pullHiForest_ALL.root",
                        "root://eoscms//eos/cms/store/group/cmst3/user/dgulhan/MultiJetSkims/20160720/ppMCpthat80+pullHiForestAOD_ALL.root",
-                        "root://eoscms//eos/cms/store/group/cmst3/user/dgulhan/MultiJetSkims/20160720/ppMCpthat80+pullHiForestAOD_ALL.root"};
+                       "root://eoscms//eos/cms/store/group/cmst3/user/dgulhan/MultiJetSkims/20160720/ppMCpthat80+pullHiForestAOD_ALL.root"};
     
-    TTree *xc_R4_N3_PF[nFiles];
+    TTree *tree[nFiles];
     TFile *file[nFiles];
     TGraphErrors * gr[nFiles];
     
-    Double_t Y0[nPoints];
-    Double_t Y0err[nPoints];
-    Double_t Y1[nPoints];
-    Double_t Y1err[nPoints];
-    Double_t Y2[nPoints];
-    Double_t Y2err[nPoints];
-    Double_t Y3[nPoints];
-    Double_t Y3err[nPoints];
+    Double_t Y[nFiles][nPoints];
+    Double_t Yerr[nFiles][nPoints];
     
+    int Color [] = { kRed , kBlue, kGreen, kMagenta };
     
-    int Color[]={kRed,kBlue,kGreen,kMagenta};
-    TH1D * h[nFiles][nPoints];
-    
+    TH1D * Hist[nFiles][nBinsCentr];
+    TTree * tree[nFiles];
 
-    int iFile;
     
-    iFile=0;
-    
-    file[iFile] = TFile::Open(Files[iFile].Data());
-    xc_R4_N3_PF[iFile]= (TTree*)file[iFile]->Get("xc_R4_N3_PF");
-    //xc_R4_N3_PF[iFile]= (TTree*)file[iFile]->Get("ak4PF");
-
-    for ( int iPoint = 0 ; iPoint< nPoints ; iPoint++){
+    for ( int iFile = 0 ; iFile < nFiles ; iFile ++){
         
-        h[iFile][iPoint] = new TH1D(Form("h%i%i",iFile,iPoint),"",50,0,3.5);
-        xc_R4_N3_PF[iFile]->Draw(Form("sqrt( pow(pullEta2,2.0) + pow(pullPhi2,2.0) )>>h%i%i",iFile,iPoint),CutSeePull[0] && CutsR[iPoint] && centrality);
-        Y0[iPoint] = h[iFile][iPoint]->GetMean();
-        cout<<h[iFile][iPoint]->GetMean()<<endl;
-        Y0err[iPoint] = h[iFile][iPoint]->GetMeanError();
+        
+        cout<<"loading file:"<<Files[iFile].Data()<<endl;
+        file[iFile] = TFile::Open( Files[iFile].Data() );
+        tree[iFile] = (TTree*)file[iFile]->Get(Form("xc_R%i_N%i_PF",R[0],N[0]));
+        
+        for ( int iPoint = 0 ; iPoint< nPoints ; iPoint++){
+            
+            Hist[iFile][iPoint] = new TH1D(Form("h%i%i",iFile,iPoint),"",50,0,3.5);
+            
+            if (iFile == 0 || iFile ==1 )
+            {
+                tree[iFile]->Draw(Form("sqrt( pow(pullEta2,2.0) + pow(pullPhi2,2.0) )>>h%i%i",iFile,iPoint),CutSeePull[0] && CutsR[iPoint] && centrality);
+            }
+            if (iFile == 2 || iFile == 3 )
+            {
+                tree[iFile]->Draw(Form("sqrt( pow(pullEta2,2.0) + pow(pullPhi2,2.0) )>>h%i%i",iFile,iPoint),CutSeePull[0] && CutsR[iPoint]);
+            }
+            
+            Y[iFile][iPoint] = Histl[iFile][iPoint]->GetMean();
+            Yerr[iFile][iPoint] = Histl[iFile][iPoint]->GetMeanError();
+        }
     }
     
-    iFile=1;
-    
-    file[iFile] = TFile::Open(Files[iFile].Data());
-    xc_R4_N3_PF[iFile]= (TTree*)file[iFile]->Get("xc_R4_N3_PF");
-    //xc_R4_N3_PF[iFile]= (TTree*)file[iFile]->Get("ak4PF");
-
-    for ( int iPoint = 0 ; iPoint< nPoints ; iPoint++){
-        
-        h[iFile][iPoint] = new TH1D(Form("h%i%i",iFile,iPoint),"",50,0,3.5);
-        xc_R4_N3_PF[iFile]->Draw(Form("sqrt( pow(pullEta2,2.0) + pow(pullPhi2,2.0) )>>h%i%i",iFile,iPoint),CutSeePull[0] && CutsR[iPoint] );
-        Y1[iPoint] = h[iFile][iPoint]->GetMean();
-        cout<<h[iFile][iPoint]->GetMean()<<endl;
-        Y1err[iPoint] = h[iFile][iPoint]->GetMeanError();
-    }
-    
-    iFile=2;
-    
-    file[iFile] = TFile::Open(Files[iFile].Data());
-    xc_R4_N3_PF[iFile]= (TTree*)file[iFile]->Get("xc_R4_N3_PF");
-    //xc_R4_N3_PF[iFile]= (TTree*)file[iFile]->Get("ak4PF");
-
-    for ( int iPoint = 0 ; iPoint< nPoints ; iPoint++){
-        
-        h[iFile][iPoint] = new TH1D(Form("h%i%i",iFile,iPoint),"",50,0,3.5);
-        xc_R4_N3_PF[iFile]->Draw(Form("sqrt( pow(pullEta2,2.0) + pow(pullPhi2,2.0) )>>h%i%i",iFile,iPoint),CutSeePull[0] && CutsR[iPoint] );
-        Y2[iPoint] = h[iFile][iPoint]->GetMean();
-        cout<<h[iFile][iPoint]->GetMean()<<endl;
-        Y2err[iPoint] = h[iFile][iPoint]->GetMeanError();
-    }
-    
-    iFile=3;
-    
-    file[iFile] = TFile::Open(Files[iFile].Data());
-    xc_R4_N3_PF[iFile]= (TTree*)file[iFile]->Get("xc_R4_N3_PF");
-    //xc_R4_N3_PF[iFile]= (TTree*)file[iFile]->Get("ak4PF");
-
-    for ( int iPoint = 0 ; iPoint< nPoints ; iPoint++){
-        
-        h[iFile][iPoint] = new TH1D(Form("h%i%i",iFile,iPoint),"",50,0,3.5);
-        xc_R4_N3_PF[iFile]->Draw(Form("sqrt( pow(pullEta2,2.0) + pow(pullPhi2,2.0) )>>h%i%i",iFile,iPoint),CutSeePull[0] && CutsR[iPoint] );
-        Y3[iPoint] = h[iFile][iPoint]->GetMean();
-        cout<<h[iFile][iPoint]->GetMean()<<endl;
-        Y3err[iPoint] = h[iFile][iPoint]->GetMeanError();
-    }
-    
-        
     
     
     TCanvas *c2 = new TCanvas ("c2","",610,600);
-
-    gr[0] = new TGraphErrors(nPoints,X,Y0,Xerr,Y0err);
-    gr[0]->GetXaxis()->SetTitle("#Delta R_{2,3} Cut");
-    gr[0]->GetYaxis()->SetTitleOffset(1.4);
-    gr[0]->GetYaxis()->SetTitle("#LT | #vec{t} | #GT ");
-    gr[0]->SetFillColor(Color[0]);
-    gr[0]->SetLineColor(Color[0]);
-    gr[0]->Draw();
-
-    gr[1] = new TGraphErrors(nPoints,X,Y1,Xerr,Y1err);
-    gr[1]->SetFillColor(Color[1]);
-    gr[1]->SetLineColor(Color[1]);
-    gr[1]->Draw("SAME");
-    
-    gr[2] = new TGraphErrors(nPoints,X,Y2,Xerr,Y2err);
-    gr[2]->SetFillColor(Color[2]);
-    gr[2]->SetLineColor(Color[2]);
-    gr[2]->Draw("SAME");
-    
-    gr[3] = new TGraphErrors(nPoints,X,Y3,Xerr,Y3err);
-    gr[3]->SetFillColor(Color[3]);
-    gr[3]->SetLineColor(Color[3]);
-    gr[3]->Draw("SAME");
-
+    TLegend *t3=new TLegend(0.50,0.64,0.83,0.77);
+    t3->SetFillColor(0);
+    t3->SetBorderSize(0);
+    t3->SetFillStyle(0);
+    t3->SetTextFont(43);
+    t3->SetTextSize(18);
+    TString LabelGraph[] = {"PbPb PYTHIA+HYDJET", "PbPb Data" , "pp PYTHIA", "pp Data"};
     
     
-    TLegend *t2=new TLegend(0.15,0.75,0.25,0.88);
-    t2->SetFillColor(0);
-    t2->SetBorderSize(0);
-    t2->SetFillStyle(0);
-    t2->SetTextFont(43);
-    t2->SetTextSize(19);
-    t2->AddEntry(gr[0],"PbPb PYTHIA+HYDJET","l");
-    //t2->AddEntry(gr[1],"PbPb Data","l");
-    t2->AddEntry(gr[2],"pp PYTHIA","l");
-    //t2->AddEntry(gr[3],"pp Data","l");
-    t2->Draw("SAME");
-    drawText("p_{T,1}>180 GeV  p_{T,2}>70 GeV p_{T,3}>70 GeV |#Delta#phi_{1,2}|>2/3 #pi",0.25,0.20,15);
+    for ( int iFile = 0 ; iFile < nFiles ; iFile ++){
+        gr[iFile] = new TGraphErrors(nPoints,X,Y[iFile],Xerr,Yerr[iFile]);
+        gr[iFile]->SetFillColor(Color[iFile]);
+        gr[iFile]->SetLineColor(Color[iFile]);
+        if (iFile == 0) {
+            gr[iFile]->GetXaxis()->SetTitle("#Delta R_{2,3} Cut");
+            gr[iFile]->GetYaxis()->SetTitleOffset(1.4);
+            gr[iFile]->GetYaxis()->SetTitle("#LT | #vec{t} | #GT ");
+            gr[iFile]->Draw();
+        }
+        else{
+            gr[iFile]->Draw("SAME");
+        }
+        
+        t3->AddEntry(gr[iFile],LabelGraph[iFile],"l");
+    }
 
-
+    t3->Draw("SAME");
+    drawText("CMS Preliminary",0.12,0.85,23);
+    drawText("p_{T,1}>180 GeV  p_{T,2}>70 GeV p_{T,3}>70 GeV",0.13,0.82,16);
+    drawText("|#Delta#phi_{2,3}|>2#pi/3 |#Delta#eta_{2,3}|>0.2",0.13,0.78,16);
     
+    
+    
+    
+    c2->SaveAs("totbalvscentrality.png");
+    c2->SaveAs("totbalvscentrality.pdf");
+    c2->SaveAs("totbalvscentrality.gif");
+    
+    
+
     
     
 
