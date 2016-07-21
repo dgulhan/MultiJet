9fc5c19ecbda894899cc2ebb2a4b79469fed6f54
Edna Ruiz <eruiz@ednasmac.dyndns.cern.ch>
2016-07-21 17:30:01 +0200

    mean pull

--- mean_pull.C
+++ mean_pull.C
@@ -120,8 +120,12 @@ void mean_pull(){
         gr[iFile]->SetLineColor(Color[iFile]);
         if (iFile == 0) {
             gr[iFile]->GetXaxis()->SetTitle("#Delta R_{2,3} Cut");
-            gr[iFile]->GetYaxis()->SetTitleOffset(1.4);
+            gr[iFile]->GetYaxis()->SetTitleOffset(2.4);
+            gr[iFile]->SetMinimum(0.01);
+            gr[iFile]->SetMaximum(0.0.0055+0.000001);
             gr[iFile]->GetYaxis()->SetTitle("#LT | #vec{t} | #GT ");
+            gr[iFile]->SetMinimum(0.11);
+            gr[iFile]->SetMaximum(0.30);
             gr[iFile]->Draw();
         }
         else{
