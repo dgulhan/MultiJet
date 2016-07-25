c7b3f6fdcce62d94132c3717eaa081df7eebc023
Edna Ruiz <eruiz@ednasmac.dyndns.cern.ch>
2016-07-25 13:52:58 +0200

    sigma smearing

--- SigmaSmearing.C
+++ SigmaSmearing.C
@@ -92,7 +92,7 @@ void SigmaSmearing(){
     
     for (int iFile = 0; iFile < nFiles ; iFile++ ) {
         
-        corr[iFile] = new TH2D (Form("corr%i",iFile),"",50,-0.1,0.1,50,-0.01,0.01);
+        corr[iFile] = new TH2D (Form("corr%i",iFile),"",50,-0.5,0.5,50,0.0,TMath::Pi());
         tree[iFile]->Draw(Form("magnitude(pullEta2,pullPhi2):(theta23-reftheta23)>>corr%i",iFile),Cut[iFile]);
         
         
