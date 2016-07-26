7e4b5f723823ad53afbf58c8ed236f7496bd8338
Edna Ruiz <eruiz@ednasmac.dyndns.cern.ch>
2016-07-26 12:04:11 +0200

    sigma smearing

--- SigmaSmearing.C
+++ SigmaSmearing.C
@@ -217,7 +217,7 @@ void SigmaSmearing(){
         
         for (int iCentr=0; iCentr<nCentr; iCentr++) {
                 
-            hist[iFile][iCentr] =  new TH1D(Form("hist%i%i",iFile,iCentr),";|#vec{t}_{raw}|-|#vec{t}_{ref}|;Event Fraction",50,-0.025,0.025);
+            hist[iFile][iCentr] =  new TH1D(Form("hist%i%i",iFile,iCentr),";|#vec{t}_{raw}|-|#vec{t}_{ref}|;Event Fraction",50,-0.015,0.015);
                 
             if (iFile==0)  tree[iFile]->Draw(Form("dt>>hist%i%i",iFile,iCentr),Cut[iFile] && CentralityBinsCuts[iCentr]);
             if (iFile == 1) tree[iFile]->Draw( Form("dt>>hist%i%i",iFile,iCentr),Cut[iFile] );
