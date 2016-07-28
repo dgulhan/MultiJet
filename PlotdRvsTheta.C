3b768486ff42b94c1249334f66efdd415db0b29a
Edna Ruiz <eruiz@ednasmac.dyndns.cern.ch>
2016-07-28 14:19:01 +0200

    5 points

--- PlotdRvsTheta.C
+++ PlotdRvsTheta.C
@@ -164,8 +164,8 @@ void PlotdRvsTheta(){
             gr[iFile]->GetXaxis()->SetTitle("#Delta R_{2,3} Cut");
             gr[iFile]->GetYaxis()->SetTitleOffset(2.4);
             gr[iFile]->SetMinimum(0.20);
-            gr[iFile]->SetMaximum(1.51);
-            gr[iFile]->GetYaxis()->SetTitle("#theta_{2,3}^{Pull}[0,#pi/2]/ #theta_{2,3}^{Pull}[#pi/2,#pi] ");
+            gr[iFile]->SetMaximum(1.66);
+            gr[iFile]->GetYaxis()->SetTitle("#theta_{2,3}^{Pull}[0,#pi/2]/ #theta_{2,3}^{Pull}[0,#pi] ");
             
             gr[iFile]->Draw();
         }
