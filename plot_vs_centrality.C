803e19c917449fa5d0755bc6d1df3c16c6cd2bee
Edna Ruiz <eruiz@ednasmac.dyndns.cern.ch>
2016-07-28 19:49:40 +0200

    plot vs centr

--- plot_vs_centrality.C
+++ plot_vs_centrality.C
@@ -78,7 +78,7 @@ void plot_vs_centrality(){
     
     
     TCanvas *c = new TCanvas ("c","",610,600);
-    c->SetLeftMargin(0.35);
+    c->SetLeftMargin(0.20);
     
     
     TLegend *t3=new TLegend(0.55,0.63,0.88,0.78);
@@ -95,7 +95,7 @@ void plot_vs_centrality(){
         gr[iFile] = new TGraphErrors(nBinsCentr,X,Y[iFile],Xerr,Yerr[iFile]);
         gr[iFile]->GetXaxis()->SetLimits(0,100);
         gr[iFile]->GetXaxis()->SetTitle("% Centrality ");
-        gr[iFile]->GetYaxis()->SetTitle("#LT P_{T} Balance #GT");
+        //gr[iFile]->GetYaxis()->SetTitle("#LT P_{T} Balance #GT");
         
         if (iFile == 0) {
             gr[iFile]->SetMinimum(0.10);
@@ -131,9 +131,9 @@ void plot_vs_centrality(){
     t3->AddEntry(c,"anti-k_{T} R=0.4","");
 
     t3->Draw("SAME");
-    drawText("CMS Preliminary",0.30,0.85,23);
-    drawText("p_{T,1}>140 GeV  p_{T,2}>50 GeV p_{T,3}>50 GeV",0.30,0.82,16);
-    drawText("|#Delta#phi_{2,3}|>2#pi/3 |#Delta#eta_{2,3}|>0.2",0.30,0.78,16);
+    drawText("CMS Preliminary",0.20,0.85,23);
+    drawText("p_{T,1}>140 GeV  p_{T,2}>50 GeV p_{T,3}>50 GeV",0.20,0.82,16);
+    drawText("|#Delta#phi_{2,3}|>2#pi/3 |#Delta#eta_{2,3}|>0.2",0.20,0.78,16);
 
     
     
