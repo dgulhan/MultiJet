72fc14e54f6ac55c5a99aafbad80cef403665f79
Doga <dgulhan@cern.ch>
2016-07-29 13:28:12 +0200

    fix fit param

--- SigmaSmearing.C
+++ SigmaSmearing.C
@@ -227,8 +227,9 @@ void SigmaSmearing(){
 			float maxBinCenter = hist[iFile][iCentr]->GetBinCenter(hist[iFile][iCentr]->GetMaximumBin());
 			float histRMS = hist[iFile][iCentr]->GetRMS();
 			
-			func[iFile][iCentr] = new TF1(Form("func_%d_%d", iFile, iCentr),"gaus(0)", maxBinCenter - 2*histRMS, maxBinCenter + 2*histRMS);
-			func[iFile][iCentr]->SetParameters(0.1,0.,0.5);
+			if(iFile==0) func[iFile][iCentr] = new TF1(Form("func_%d_%d", iFile, iCentr),"gaus(0)", maxBinCenter - 2*histRMS, maxBinCenter + 2*histRMS);
+			else func[iFile][iCentr] = new TF1(Form("func_%d_%d", iFile, iCentr),"gaus(0)", maxBinCenter - histRMS, maxBinCenter + histRMS);
+			func[iFile][iCentr]->SetParameters(0.1,0.,0.01);
 			hist[iFile][iCentr]->Fit(func[iFile][iCentr],"R LL");
                 
             // func[iFile][iCentr] = (TF1*)hist[iFile][iCentr]->GetFunction("gaus");
