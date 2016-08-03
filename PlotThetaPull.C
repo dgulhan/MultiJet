9bdf05d0d74ebd2a599baf1ba4729669fe174ce9
Edna Ruiz <eruiz@ednasmac.dyndns.cern.ch>
2016-08-03 16:46:49 +0200

    plot wo data

--- PlotThetaPull.C
+++ PlotThetaPull.C
@@ -122,7 +122,7 @@ void PlotThetaPull(){
             cout<<"out of here 2"<<endl;
 
             c5->cd(iAlgo+1);
-            hist[iFile][iAlgo]->SetMaximum(0.11);
+            hist[iFile][iAlgo]->SetMaximum(0.12);
 
             if (iFile == 0 || iFile == 2) {
                 if (iFile == 0) hist[iFile][iAlgo]->SetFillStyle(3005);
